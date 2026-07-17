87b3e2add4c58b36adad5c59009ddf575d816521
CarlosM1985 <mvz.olivermartinez@gmail.com>
2026-07-16 21:51:35 -0400

    fix: session bar con avatar + safe area + historial 22 bins ordenado + sw v11

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'feedbins-colfax-v10';
+const CACHE = 'feedbins-colfax-v11';
 const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
